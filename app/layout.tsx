c6e8f01905938e4af9230447ef6b2deb7f1f3a3b
Kaishu Terahara <refai.apa7@gmail.com>
2026-04-18 17:09:43 +0900

    fix clerk layout and env setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,15 +24,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/">
-      <html
-        lang="en"
-        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      >
-        <body className="min-h-full flex flex-col bg-black text-white">
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col bg-black text-white">
+        <ClerkProvider afterSignOutUrl="/">
           {children}
-        </body>
-      </html>
-    </ClerkProvider>
+        </ClerkProvider>
+      </body>
+    </html>
   );
 }
